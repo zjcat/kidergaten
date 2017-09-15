82289a988dc0610ae6db98bd505de7802e4762d7
jinfeifei <18310653075@163.com>
2017-09-15 09:26:45 +0800

    本地兼容以及精灵图

--- src/page/common/nav-sad/index.js
+++ src/page/common/nav-sad/index.js
@@ -2,6 +2,15 @@
 * @Author: Administrator
 * @Date:   2017-09-14 09:57:30
 * @Last Modified by:   Administrator
-* @Last Modified time: 2017-09-14 12:00:14
+* @Last Modified time: 2017-09-15 09:01:24
 */
 require('./index.css');
+var page = {
+	getElem : function(){
+		$(".icon").each(function(){
+			var getElemPosion = $(this).attr("data-num");
+			$(this).css("background-position-y",-getElemPosion);
+		});
+	}
+}
+module.exports = page;
--- src/page/tour/index.js
+++ src/page/tour/index.js
@@ -2,11 +2,11 @@
 * @Author: Administrator
 * @Date:   2017-09-14 10:01:07
 * @Last Modified by:   Administrator
-* @Last Modified time: 2017-09-14 14:09:26
+* @Last Modified time: 2017-09-15 08:55:40
 */
 require('./index.css');
 require('page/common/header/index.js');
-require('page/common/nav-sad/index.js');
+var navSad = require('page/common/nav-sad/index.js');
 var _cat = require('util/interface.js');
 var navList = require('page/common/nav/index.js');
 require('page/common/footer/index.js');
@@ -14,5 +14,6 @@ $(function() {
 	//页面加载完毕才执行
     navList.init({
         name : 'tour-list'
-    })
+    });
+    navSad.getElem();
 });
